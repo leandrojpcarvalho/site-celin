d34a0e446bec6a6d5808e7ca6a9faa5044f7aadd
Leandro <ljpcarvalho@gmail.com>
2024-03-23 22:48:25 -0300

    feat: application start

---
+++ src/main/java/com/celin/sitecelin/SiteCelinApplication.java
@@ -0,0 +1,13 @@
+package com.celin.sitecelin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteCelinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteCelinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celin/sitecelin/SiteCelinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.celin.sitecelin;
+
+import com.celin.sitecelin.controller.UserController;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SiteCelinApplicationTests {
+
+	@Test
+	void userClass() {
+	}
+
+}
